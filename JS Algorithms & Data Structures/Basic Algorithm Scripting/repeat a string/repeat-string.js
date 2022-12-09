d8ff6bee60768308ed78daf454f757c3854cf318
sherms <sherms77@gmail.com>
2022-12-10 09:07:37 +1100

    modified:   JS Algorithms & Data Structures/Basic Algorithm Scripting/repeat a string/README-repeat-a-string.md 	modified:   JS Algorithms & Data Structures/Basic Algorithm Scripting/repeat a string/repeat-string.js

--- JS Algorithms & Data Structures/Basic Algorithm Scripting/repeat a string/repeat-string.js
+++ JS Algorithms & Data Structures/Basic Algorithm Scripting/repeat a string/repeat-string.js
@@ -13,10 +13,13 @@ function repeatStringNumTimes(str, num) {
   
 // console.log(repeatStringNumTimes("abc", 3))
 
-console.log(repeatStringNumTimes("*", 3)) // should return the string ***.
+// console.log(repeatStringNumTimes("*", 3)) // should return the string ***.
 // console.log(repeatStringNumTimes("abc", 3)) // should return the string abcabcabc.
 // console.log(repeatStringNumTimes("abc", 4)) // should return the string abcabcabcabc.
 // console.log(repeatStringNumTimes("abc", 1)) // should return the string abc.
-// console.log(repeatStringNumTimes("*", 8)) // should return the string ********.
+
+console.log(repeatStringNumTimes("*", 8)) // should return the string ********. -> HAS CORRECT OUTPUT BUT ALSO OUTPUTS 'UNDEFINED'
+
 // console.log(repeatStringNumTimes("abc", -2)) // should return an empty string ("").
-// console.log(repeatStringNumTimes("abc", 0)) // should return "".
+
+// WATCH - console.log(repeatStringNumTimes("abc", 0)) // should return "". -> OUTPUTS 'UNDEFINED'
