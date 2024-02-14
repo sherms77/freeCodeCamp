9a6b9e08d80ad6ae76a59365bc61cdb5674f0404
sherms <sherms77@gmail.com>
2024-02-15 07:46:56 +1100

    new file:   JS Algorithms & Data Structures/Basic Algorithm Scripting/falsey bouncer/function_add.js

---
+++ JS Algorithms & Data Structures/Basic Algorithm Scripting/falsey bouncer/function_add.js
@@ -0,0 +1,23 @@
+// why is it only adding one element at a time?
+// issue with my logic?
+
+n = [];
+// only taking one argument in function param
+// if you put params in an array in the function call it will push both elements to the new array but its "nested"
+function add_arr(new_elements) {
+    // n = [];
+    n.push(new_elements);
+    console.log(n);
+}
+
+// add_arr(1,3);
+add_arr([4,5]);
+add_arr(["apple", "lemon"]);
+add_arr(["pizza"]);
+
+// add_arr([5,6]);
+// add_arr(["element"]);
+
+// x = [7,8];
+// x.push("lemon", "pie");
+// console.log(x);
