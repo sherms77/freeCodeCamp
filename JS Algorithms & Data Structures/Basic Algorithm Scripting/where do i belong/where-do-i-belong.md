2c0fe753c81388b3ca0ea665f7a2eb2b35554254
sherms <sherms77@gmail.com>
2024-03-17 10:39:51 +1100

    modified:   JS Algorithms & Data Structures/Basic Algorithm Scripting/where do i belong/where-do-i-belong.md 	modified:   JS Algorithms & Data Structures/Basic Algorithm Scripting/where do i belong/where.js

--- JS Algorithms & Data Structures/Basic Algorithm Scripting/where do i belong/where-do-i-belong.md
+++ JS Algorithms & Data Structures/Basic Algorithm Scripting/where do i belong/where-do-i-belong.md
@@ -50,6 +50,30 @@ getIndexToIns([], 1) should return 0.
 getIndexToIns([], 1) should return a number. - unsure what output of this test should be?
 
 # notes
+- 170324: unable to get alternate condition to work where `if element is <= num -> output: num or index of element`. code i tried in fcc editor:
+```
+function getIndexToIns(arr, num) {
+  arr.sort((a, b) => a - b);
+
+  for(let i = 0; i < arr.length; i++) {
+    // let val = arr[i];
+    if (arr[i] >= num) {
+      let val = arr[i];
+      return (arr.indexOf(val));
+    } else if (arr[i] <= num) {
+      let val2 = arr[i];
+      return (arr.indexOf(val2))
+    } 
+    
+    // else {
+    //   return num;
+    // }
+
+  }
+}
+
+getIndexToIns([40, 60], 50);
+```
 - 160324: Need to add a condition to this case: If the array elements are <= 'num'.
 - 160324: updated `line 60` in my code below to `if (arr[i] >= num)`. passed 12/16 tests. didn't pass last `4` tests.
 - 150324: ran this code in fcc editor. passed 9 tests. need to analyse and debug.
--- JS Algorithms & Data Structures/Basic Algorithm Scripting/where do i belong/where.js
+++ JS Algorithms & Data Structures/Basic Algorithm Scripting/where do i belong/where.js
@@ -5,12 +5,16 @@ function getIndexToIns(arr, num) {
   // console.log("sorted array:", arr);
   
   // check if the element is bigger than 'num'
+  // let val = arr[i];
   for(i = 0; i < arr.length; i++) {
+    let val = arr[i];
     if (arr[i] >= num) {
-      let val = arr[i];
+      // let val = arr[i];
       // console.log(val);
       console.log(arr.indexOf(val)); // returns index of array element > num
-    } // 160324: NEED TO ADD A CONDITION TO HANDLE IF THE ARRAY ELEMENTS ARE <= 'NUM'.
+    } else {
+      console.log(num, val);
+    }// 160324: NEED TO ADD A CONDITION TO HANDLE IF THE ARRAY ELEMENTS ARE <= 'NUM'.
   }
 
 }
@@ -32,7 +36,6 @@ function getIndexToIns(arr, num) {
 
 // 160324: FAILING THESE TESTS IN FCC EDITOR
 getIndexToIns([2, 5, 10], 15)  // should return 3.
-getIndexToIns([2, 5], 15)
-// getIndexToIns([2, 5, 10], 15)  // should return a number.
+getIndexToIns([2, 5, 10], 15)  // should return a number.
 // getIndexToIns([], 1)  // should return 0.
 // getIndexToIns([], 1)  // should return a number. - unsure what output of this test should be?
