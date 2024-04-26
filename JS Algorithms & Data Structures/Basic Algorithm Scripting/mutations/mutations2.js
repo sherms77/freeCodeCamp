7fc0553422027c91f451ffda466ad8a663bebc36
sherms <sherms77@gmail.com>
2024-04-26 11:14:30 +1000

    modified:   JS Algorithms & Data Structures/Basic Algorithm Scripting/mutations/mutations.md 	modified:   JS Algorithms & Data Structures/Basic Algorithm Scripting/mutations/mutations2.js

--- JS Algorithms & Data Structures/Basic Algorithm Scripting/mutations/mutations2.js
+++ JS Algorithms & Data Structures/Basic Algorithm Scripting/mutations/mutations2.js
@@ -1,21 +1,26 @@
-// Hint 1: If everything is lowercase it will be easier to compare.
-// Hint 2: Our strings might be easier to work with if they were arrays of characters.
+// Hint 1: If everything is lowercase it will be easier to compare. - DONE
+// Hint 2: Our strings might be easier to work with if they were arrays of characters. - DONE BUT HOW TO COMPARE IN THIS STATE?
 // Hint 3: A loop might help. Use indexOf() to check if the letter of the second word is on the first.
 
 function mutation(arr) {
     // console.log("original array:", arr);
-    let s = arr.split("");
-    console.log(s);
+    // console.log(arr[0].toLowerCase().split(""));
+    let arr1 = arr[0].toLowerCase().split("");
+    let arr2 = arr[1].toLowerCase().split("");
+
+    console.log(arr1);
+    console.log(arr2);
 
     for(let i = 0; i < arr.length; i++) {
-        // let lower = arr[i].toLowerCase(); // convert characters in each element to lower case
-        console.log(arr[i].split)
-        // console.log(arr[i].indexOf(arr[i]));
+        console.log(arr1[i].indexOf(arr2[i]));
     }
+    
+    // console.log(j);
     // return arr;
+
   }
 
 
 // mutation(["floor", "for"]) // should return true.
-// mutation(["ate", "date"]) // should return false.
+mutation(["ate", "date"]) // should return false. letters in first element does NOT contain ALL letters in second element
 mutation(["Noel", "Ole"]) // should return true. - DOES NOT WORK??
