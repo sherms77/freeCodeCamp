ed5635f8d67e1c8838ecfb987c9d7827eefe027b
sherms <sherms77@gmail.com>
2024-04-27 13:49:58 +1000

    modified:   JS Algorithms & Data Structures/Basic Algorithm Scripting/mutations/mutations.md 	modified:   JS Algorithms & Data Structures/Basic Algorithm Scripting/mutations/mutations2.js

--- JS Algorithms & Data Structures/Basic Algorithm Scripting/mutations/mutations.md
+++ JS Algorithms & Data Structures/Basic Algorithm Scripting/mutations/mutations.md
@@ -54,6 +54,7 @@ mutation(["Tiger", "Zebra"]) should return false.
 mutation(["Noel", "Ole"]) should return true.
 
 # notes
+- 270424: refer to `mutaions2.js`. trying to loop over elements in `arr1` and `arr2` to check if all the characters in `arr2` are present in `arr1`. problem is trying to create the `paramater` for the second `argument` in the `for loop` that will loop over **all** characters in **both** arrays.
 - 260424: refer to `mutations2.js`. converted the strings in `arr` into `lower case` and then `split` the strings, in each element into seperate `characters` and saved them into their own `arrays`. need to work out how to use 
 `.indexOf()` to compare the characters to get the desired result.
 - 260424: working out how to `split` the sting into characters for each element in the the array. refer to `mutations2.js` and `split.js`.
--- JS Algorithms & Data Structures/Basic Algorithm Scripting/mutations/mutations2.js
+++ JS Algorithms & Data Structures/Basic Algorithm Scripting/mutations/mutations2.js
@@ -8,11 +8,22 @@ function mutation(arr) {
     let arr1 = arr[0].toLowerCase().split("");
     let arr2 = arr[1].toLowerCase().split("");
 
-    console.log(arr1);
-    console.log(arr2);
+    // console.log(arr1);
+    // console.log(arr2);
 
-    for(let i = 0; i < arr.length; i++) {
-        console.log(arr1[i].indexOf(arr2[i]));
+    // arr.length is the probelm. 
+    // it won't loop over the full length of arr1 and arr2 as there are only 2 elements in arr
+    // used length of arr2 to loop over elements in both arrays. 
+    // does not work in case where arr1 is shorter when applyting the 'if' statement.
+    for(let i = 0; i < arr2.length; i++) {
+        // console.log(arr1[i]);
+        // console.log(arr2[i]);
+        console.log(arr1[i].indexOf(arr2[i])); 
+
+        if (arr1[i].indexOf(arr2[i]) >= 0) {
+            console.log(true);
+        };
+        
     }
     
     // console.log(j);
@@ -23,4 +34,4 @@ function mutation(arr) {
 
 // mutation(["floor", "for"]) // should return true.
 mutation(["ate", "date"]) // should return false. letters in first element does NOT contain ALL letters in second element
-mutation(["Noel", "Ole"]) // should return true. - DOES NOT WORK??
+// mutation(["Noel", "Ole"]) // should return true. - DOES NOT WORK??
